b104f905c26d51b817fc0a51a53cf7123ee4ad3a
Ariel Vernaza <ariel.darkserver@gmail.com>
2020-06-16 21:15:48 -0500

    download csv

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
-  get 'data/:checkpoint_id', to: 'daily_weathers#download_csv', as: :download_csv
+  get 'data/:checkpoint_id', to: 'daily_weathers#download_csv', as: :download_csv, :defaults => { :format => 'csv' }
 end
